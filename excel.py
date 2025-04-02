a319a7dd82dca9ac77bcdb86d0bc31e30aad7f57
pk_red_dark_parasu <87744129+PkRedDark000@users.noreply.github.com>
2025-04-02 14:25:10 +0300

    Update excel.py

--- excel.py
+++ excel.py
@@ -1,7 +1,7 @@
 pip install openpyxl
 import openpyxl
 import os
-form openpyxl import Workbook
+from openpyxl import Workbook
 
 dir(openpyxl) #open the dir
 wb  = Workbook()   #wb = workbook
@@ -9,8 +9,20 @@ sheet = wb.active #new sheet make comment
 sheet
 sheet["A1"] = "name" #what ever u enter etc...
 sheet["B1"] = "age" #what ever u enter etc...
-wb.save(filename = "test.xlsx" #file name enter i am enter test.xlsx u enter etc...
+wb.save(filename = "test.xlsx") #file name enter i am enter test.xlsx u enter etc...
 os.getcwd() #find the dir comment
 wb.save(filename = "C:\\download\\test.xlsx") # dir path name c diver /download folder
+        
+# step 2 sheet update comment
+sheet1 =wb.create_sheet() # without name
+sheet2 =wb.create_sheet("test book") # with name
+sheet3  =wb.create_sheet("test1 book") # with name
+sheet4  =wb.create_sheet("test2 book",0) # with name with opstion
+sheet5  =wb.create_sheet("test2 book",1) # with name with opstion
+
+
+
+
+
 
 
